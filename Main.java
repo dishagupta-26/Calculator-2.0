7373d9f6fd12d53bd66a730496d4e9d36a8ecc50
Disha Gupta <gupta.disha.d26@gmail.com>
2025-04-13 21:16:30 +0530

    initialize main method with user input setup

--- Main.java
+++ Main.java
@@ -6,6 +6,12 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        double a, b;
+        while (true) {
+
+        }
 
     }
 
